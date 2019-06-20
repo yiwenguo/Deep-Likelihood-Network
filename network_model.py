fd68df95b73d6901bad3acbd40553463ee369a8b
Yiwen GUO <guoyiwen89@163.com>
2019-06-20 18:12:35 +0800

    Add files via upload

--- network_model.py
+++ network_model.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def generate_mask(batch_size, image_shape, inpaint_size_range, inpaint_shift_range):    
+def generate_inpainting_mask(batch_size, image_shape, inpaint_size_range, inpaint_shift_range):    
     random_inpaint_size = tf.random_uniform([batch_size,], 
                                             minval=inpaint_size_range[0], 
                                             maxval=inpaint_size_range[1] + 1.0) 
@@ -24,6 +24,18 @@ def generate_mask(batch_size, image_shape, inpaint_size_range, inpaint_shift_ran
                     tf.expand_dims(v + mask_width, 1), dtype=tf.float32)
     image_mask = tf.expand_dims(hmask, 2) * tf.expand_dims(vmask, 1)
     image_mask = tf.reshape(image_mask, [batch_size,] + image_shape[:2] + [1,])
+
+    return image_mask
+
+
+def generate_interpolation_mask(batch_size, image_shape, interpo_perc_range):    
+    random_inpaint_perc = tf.random_uniform([batch_size,1,1,1], 
+                                            minval=interpo_perc_range[0], 
+                                            maxval=interpo_perc_range[1]) 
+
+    image_mask = tf.random_uniform([batch_size,]+image_shape[:2]+[1,], 
+                                   minval=0.0, maxval=1.0)
+    image_mask = tf.cast(image_mask <= random_inpaint_perc, dtype=tf.float32)
 
     return image_mask
 
@@ -114,6 +126,62 @@ def inpainting_net(Inputs, Weights, first_iter=False, previous_z_dec=None, is_tr
     }
 
     return input_tensors, output_tensors
+
+
+def interpolation_net(Inputs, Weights, first_iter=False, previous_z_dec=None, is_train=False):
+    
+    input_images, perc_range, image_mask = Inputs  
+    masked_images = input_images * (1.0 - image_mask)
+    input_shape = input_images.get_shape().as_list()
+    batch_size = input_shape[0]
+
+    # The context encoder
+    if first_iter:
+        z = masked_images
+        for i in xrange(4):                          
+            z_enc = tf.nn.conv2d(z, Weights[2*i], strides=[1, 2, 2, 1], padding='SAME')
+            z_enc = tf.layers.batch_normalization(z_enc, 
+                training=is_train, name='bn%s_enc' %i)
+            z = tf.nn.leaky_relu(z_enc, 0.2)   
+        z = tf.transpose(tf.reshape(z, [batch_size, -1, 512]), [2, 0, 1])
+        z = tf.reshape(tf.matmul(z, Weights[2*4]), [512, batch_size, 4, 4])
+        z = tf.transpose(z, [1, 2, 3, 0]) 
+        for i in xrange(3):              
+            shape = [batch_size,8*(2**i),8*(2**i),Weights[2*i+10].get_shape().as_list()[-2]]
+            z_dec = tf.nn.conv2d_transpose(z, Weights[2*i+10], 
+                output_shape=shape, strides=[1, 2, 2, 1], padding='SAME') 
+            z_dec = tf.layers.batch_normalization(z_dec, 
+                training=is_train, name='bn%s_dec' %i)
+            z = tf.nn.relu(z_dec)    
+    else:
+        assert previous_z_dec is not None
+        z_dec = previous_z_dec
+        z = tf.nn.relu(z_dec)
+
+    # The estimation
+    fake_images = tf.nn.conv2d_transpose(z, Weights[-2], 
+        output_shape=input_shape, strides=[1, 2, 2, 1], padding='SAME')        
+
+    eucd_loss = tf.reduce_sum(tf.square(fake_images - input_images))/batch_size/2 
+    like_loss = tf.reduce_sum(tf.square(fake_images - input_images)*(1.0 - image_mask)  \
+        /tf.reduce_sum(1.0 - image_mask, [1, 2, 3], keepdims=True))/batch_size/2
+    conv_vars = [var for var in tf.trainable_variables() if '/conv' in var.name]              
+    norm_loss = tf.reduce_sum(1e-4 * tf.stack([tf.nn.l2_loss(i) for i in conv_vars]))    
+
+    input_tensors = {
+        'images' : input_images,
+        'interpo_perc_range' : perc_range,
+    }
+
+    output_tensors = {
+        'fake_images' : fake_images,
+        'fake_hidden' : z_dec,
+        'like_loss' : like_loss,
+        'eucd_loss' : eucd_loss,
+        'norm_loss' : norm_loss,
+    }
+
+    return input_tensors, output_tensors
     
 
 def inpainting_DLNet(options): 
@@ -126,7 +194,7 @@ def inpainting_DLNet(options):
         input_images = tf.placeholder('float32', [batch_size,] + image_shape, name='images')
         size_range = tf.placeholder('float32', [2,], name='inpaint_size_interval')
         shift_range = tf.placeholder('float32', [2,], name='inpaint_shift_interval')
-        image_mask = generate_mask(batch_size, image_shape, size_range, shift_range)
+        image_mask = generate_inpainting_mask(batch_size, image_shape, size_range, shift_range)
         Inputs = [input_images, size_range, shift_range, image_mask]
 
         # Prepare network weights
@@ -142,6 +210,46 @@ def inpainting_DLNet(options):
         previous_z_dec=None 
         for i in xrange(options['k']):
             input_tensors, output_tensors = inpainting_net(Inputs, Weights,  
+                first_iter, previous_z_dec, options['phase']=='Train')
+
+            # Adam solver for maximizing the likelihood
+            Dlr_z = tf.gradients(output_tensors['like_loss'], output_tensors['fake_hidden'])[0]
+            MDlr_z = beta1*MDlr_z + (1-beta1)*Dlr_z    
+            VDlr_z = beta2*VDlr_z + (1-beta2)*(Dlr_z*Dlr_z)    
+            MDlr_z_cap = MDlr_z/(1-(beta1**(i+1)))        
+            VDlr_z_cap = tf.maximum(VDlr_z/(1-(beta2**(i+1))), gamma)        
+            previous_z_dec = output_tensors['fake_hidden'] - \
+                (lr*MDlr_z_cap)/tf.sqrt(VDlr_z_cap)           
+            first_iter=False
+
+        return input_tensors, output_tensors
+
+
+def interpolation_DLNet(options): 
+    with tf.variable_scope('net_interpolation', reuse=options['reuse']):    
+
+        batch_size = options['batch_size']
+        image_shape = options['image_shape']        
+        
+        # Prepare inputs
+        input_images = tf.placeholder('float32', [batch_size,] + image_shape, name='images')
+        perc_range = tf.placeholder('float32', [2,], name='interpolation_perc')
+        image_mask = generate_interpolation_mask(batch_size, image_shape, perc_range)
+        Inputs = [input_images, perc_range, image_mask]
+
+        # Prepare network weights
+        feature_dims = [3, 64, 128, 256, 512]        
+        Weights = generate_weights(feature_dims)
+        
+        # Prepare hyper-parameters for Adam
+        VDlr_z, MDlr_z = 0, 0
+        lr, beta1, beta2, gamma = 1e-2, 0.9, 0.999, 1e-16  
+
+        # Recursively opt for a reasonable z_dec
+        first_iter=True
+        previous_z_dec=None 
+        for i in xrange(options['k']):
+            input_tensors, output_tensors = interpolation_net(Inputs, Weights,  
                 first_iter, previous_z_dec, options['phase']=='Train')
 
             # Adam solver for maximizing the likelihood
---
+++ train_interpo.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+import sys
+import argparse
+import tensorflow as tf
+import numpy as np
+
+from random import sample
+from os import listdir, environ
+from os.path import isfile, join
+from skimage import img_as_float, img_as_ubyte
+from network_model import interpolation_DLNet
+from custom_ops import custom_psnr
+
+
+# Collect all training, validation and test datafiles
+def collect_datafiles(data_path):
+    train_datafiles = [f for f in listdir(data_path) 
+        if isfile(join(data_path, f)) and f[:len('train')]=='train']
+    test_datafiles = [f for f in listdir(data_path) 
+        if isfile(join(data_path, f)) and f[:len('test')]=='test']
+    return sample(train_datafiles, len(train_datafiles)), test_datafiles
+
+
+# Load data in specific .npz file
+def load_data(data_path, filename, shuffle=False):
+    npzfile = np.load(join(data_path, filename), 'r') 
+    image_count = len(npzfile['images'])
+    image_array = sample(npzfile['images'], image_count) \
+        if shuffle else npzfile['images']
+    return image_array
+    
+
+# Collect a minibatch of data 
+def get_batch(images, batch_k, batch_size):
+    return images[batch_k * batch_size : (batch_k + 1) * batch_size]
+
+
+def main(args):
+    
+    # environ['CUDA_VISIBLE_DEVICES']=''
+    if args.model_path is not None and args.from_scratch:
+        print ('Sure you want to train from scratch?')
+
+    # Collect datafiles and setup training model
+    filenames, test_filenames = collect_datafiles(args.data_path)
+    model_options = {
+        'batch_size' : args.batch_size,
+        'image_shape' : [args.patch_size,]*2 + [3,],
+        'phase' : 'Train',
+        'reuse' : False,
+        'k': args.k,
+    }    
+    # Input and output of the training model
+    input_tensors, output_tensors = interpolation_DLNet(model_options)
+
+    # Setup learning rate
+    global_step = tf.Variable(0, trainable=False)     
+    global_step_op = tf.assign(global_step, global_step + 1)  
+    learning_rate = tf.train.exponential_decay(
+        args.learning_rate, 
+        global_step=global_step,
+        decay_steps=args.decay_interval, 
+        decay_rate=args.decay_rate, 
+        staircase=True
+    )        
+
+    # Setup optimizer
+    all_vars = [var for var in tf.trainable_variables() 
+        if var.name.startswith('net_')]        
+    all_loss = output_tensors['eucd_loss'] + output_tensors['norm_loss']
+    optimizer = tf.train.AdamOptimizer(learning_rate)    
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+        train_op = optimizer.minimize(all_loss, var_list=all_vars) 
+        
+    # # Collect test data and setup test model
+    # assert len(test_filenames)==1
+    # test_images = load_data(args.data_path, test_filenames[0], shuffle=False)
+    # test_model_options = {
+    #     'batch_size' : args.test_batch_size,
+    #     'image_shape' : [args.patch_size,]*2 + [3,],
+    #     'phase' : 'Test',
+    #     'reuse' : True,
+    #     'k': args.k,
+    # }    
+    # # Input and output of the test model
+    # test_input_tensors, test_output_tensors = interpolation_DLNet(test_model_options) 
+
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:   
+        sess.run(tf.global_variables_initializer())
+
+        # Load pre-trained model for a single degradation setting
+        if args.from_scratch is False:
+            pretrained_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='net_')
+            pre_saver = tf.train.Saver(pretrained_vars)
+            pre_saver.restore(sess, join(args.model_path, 'model.ckpt'))          
+
+        saver = tf.train.Saver()
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord) 
+
+        for i in xrange(args.max_epoch):            
+            batch_no_cum, eucd_loss_cum = 0, 0
+            for j in xrange(len(filenames)):
+                batch_no = 0                
+                images = load_data(args.data_path, filenames[j], shuffle=True)
+                while (batch_no+1)*args.batch_size <= len(images):
+                    image_array = get_batch(images, batch_no, args.batch_size)
+                    eucd_loss = sess.run([train_op, output_tensors['eucd_loss']], 
+                    feed_dict = {
+                        input_tensors['images'] : img_as_float(image_array),
+                        input_tensors['interpo_perc_range'] : [0.0, 0.75],
+                    })[1]
+                    batch_no += 1
+                    eucd_loss_cum += eucd_loss
+                batch_no_cum += batch_no
+            filenames = sample(filenames, len(filenames))
+
+            if (i+1) % args.log_interval == 0:
+                print ('Epoch: %d; Training Euclidean loss: %s.' 
+                       %(i+1, str(eucd_loss_cum/batch_no_cum)))
+            
+            if (i+1) % args.snapshot_interval == 0:
+                print ("Saving model .............")
+                saver.save(sess, args.ckt_dir+'/model_%d.ckpt' %(i+1))
+
+            # # Test is performed in test_context_encoder.py
+            # if (i+1) % args.test_interval == 0:
+            #     test_batch_no, test_eucd_loss_cum, test_psnr_cum = 0, 0, 0
+            #     while (test_batch_no+1)*args.test_batch_size <= len(test_images):
+            #         test_image_array = get_batch(test_images, test_batch_no, args.test_batch_size)
+            #         fake_image_array, test_eucd_loss  = \
+            #         sess.run([test_output_tensors['fake_images'],
+            #                   test_output_tensors['eucd_loss']], 
+            #         feed_dict = {
+            #             test_input_tensors['images'] : img_as_float(test_image_array),
+            #             test_input_tensors['interpo_perc_range'] : [0.75, 0.75],
+            #         })                    
+            #         test_batch_no += 1
+            #         test_eucd_loss_cum += test_eucd_loss
+            #         fake_image_array = np.maximum(np.minimum(fake_image_array, 1.0), 0.0)
+            #         fake_image_array = img_as_ubyte(fake_image_array)
+            #         test_psnr_cum += np.sum(custom_psnr(fake_image_array, test_image_array))
+            #     print ('Test Euclidean loss: %s, PSNR: %s.' %(str(test_eucd_loss_cum/test_batch_no), 
+            #            str(test_psnr_cum/test_batch_no/args.test_batch_size)))
+
+            sess.run(global_step_op)
+            sys.stdout.flush()            
+
+        coord.request_stop()
+        coord.join(threads)
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('k', type=int,
+        help='Number of gradient descent steps for updating z.', default=5)    
+    parser.add_argument('--max_epoch', type=int,
+        help='Number of training epochs.', default=500)
+    parser.add_argument('--snapshot_interval', type=int, 
+        help='Snapshot per X epochs during training.', default=100)
+    parser.add_argument('--decay_interval', type=int,
+        help='Decay per X epochs during training.', default=200)
+    parser.add_argument('--test_interval', type=int, 
+        help='Test per X epochs during training.', default=10)
+    parser.add_argument('--log_interval', type=int, 
+        help='Log per X epochs during training.', default=1)
+    parser.add_argument('--patch_size', type=int,
+        help='Patch height/width in pixels.', default=64)
+    parser.add_argument('--batch_size', type=int,
+        help='Number of samples per minibatch for training.', default=100)
+    parser.add_argument('--test_batch_size', type=int, 
+        help='Number of samples per minibatch for test.', default=50)
+    parser.add_argument('--learning_rate', type=float,
+        help='Initial learning rate', default=1e-3)
+    parser.add_argument('--decay_rate', type=float,
+        help='Learning rate decay rate', default=1e-1)
+    parser.add_argument('--momentum', type=float, 
+        help='Solver momentum', default=0.9)
+    parser.add_argument('--from_scratch', 
+        help='Let deep likelihood net being trained from scratch.', action='store_true')
+
+    parser.add_argument('--model_path', type=str,
+        help='Directory for your pretrained model.', default=None)  #'./pre_trained/ckts') 
+    parser.add_argument('--data_path', type=str,
+        help='Directory for training and test data.', default='./data/CelebA/numpy/')
+    parser.add_argument('--ckt_dir', type=str,
+        help='Directory for ckt files.', default='ckts')
+
+    return parser.parse_args(argv)
+
+
+if __name__ == "__main__":
+    main(parse_arguments(sys.argv[1:]))
